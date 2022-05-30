7af5b1ef79f81eb036d588a1ad4c92f026aa0a92
BigTomatoKim <741852073@qq.com>
2022-05-30 14:53:08 +0800

    格式化代码

--- word2vec/skip_gram.py
+++ word2vec/skip_gram.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-from word2vec.vocabs import getvocabsOnlyIndex
+from vocabs import getvocabsOnlyIndex
 
 
 class Skip_Gram(nn.Module):
@@ -11,7 +11,7 @@ class Skip_Gram(nn.Module):
         vocab_numbers = len(vocabs)
         self.word_embs = nn.Embedding(vocab_numbers, vector_size)
         self.bkp_word_embs = nn.Embedding(vocab_numbers, vector_size)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.word_embs(x)
@@ -27,20 +27,29 @@ def word2vec(seqs, window_size=1):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
     net.train()
+    # s1 = [0, 1, 2, 3, 4]
+    # s2 = [0, 2, 4, 5, 6]
+    # s3 = [2, 3, 4, 4, 6]
+    # s4 = [1, 3, 5, 0, 3]
     for seq in seqs:
         for i in range(0, len(seq) - (window_size * 2)):
             optimizer.zero_grad()
             window = seq[i:i + 1 + window_size * 2]
+            print(f'window: {window}')
             # [window*2]
             x = torch.LongTensor([window[window_size] for _ in range(window_size * 2)])
+            print(f'x: {x}')
             y_pred = net(x)
+            # print(f'y_pred: {y_pred}')
             window.pop(window_size)
             # [window*2]
             y = torch.LongTensor(window)
+            print(f'y: {y}')
             loss = criterion(y_pred, y)
-            print(loss)
+            # print(loss)
             loss.backward()
             optimizer.step()
+        break
 
 
 if __name__ == '__main__':
@@ -49,4 +58,4 @@ if __name__ == '__main__':
     s3 = [2, 3, 4, 4, 6]
     s4 = [1, 3, 5, 0, 3]
     seqs = [s1, s2, s3, s4]
-    word2vec(seqs)
+    word2vec(seqs, window_size=1)
